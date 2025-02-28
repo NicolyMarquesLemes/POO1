e08331e43c50652e06ecb669303ea850de95c2d8
NicolyMarquesLemes <nicoly.lemes08@gmail.com>
2025-02-28 17:50:34 -0300

    Exercicio 8 pronto

--- Exerc.8/src/exerc/pkg8/Exerc8.java
+++ Exerc.8/src/exerc/pkg8/Exerc8.java
@@ -1,10 +1,12 @@
+package atividade8;
 
-package exerc.pkg8;
-
-public class Exerc8 {
-
+public class Atividade8 {
+   
     public static void main(String[] args) {
-       
+        Porta objporta1 = new Porta(true);
+        objporta1.abrir();
+        objporta1.fechar();
+        objporta1.fechar();
+        objporta1.abrir();
     }
-    
 }
